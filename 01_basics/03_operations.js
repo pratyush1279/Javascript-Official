d55da602607bfa1cfb0fb74f11e4469adfa4326f
pratyush1279 <pratyushpatwal@gmail.com>
2025-10-25 13:41:20 +0530

    Initial commit

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,40 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello "
+let str2 = "PP"
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+// preincreament
+
+let a = 5
+
+let b = a++
+
+console.log(a,b)   //  output a = 6 ; b = 5
+
+// postincreament
+
+let x = 6
+
+let y = ++x
+
+console.log(x,y)   // output x = 7 ; y = 7
+
+
+
+
+
+
