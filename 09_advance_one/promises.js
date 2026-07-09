af225e3984fd32b864973429e425bc16e715fca8
pratyush1279 <pratyushpatwal@gmail.com>
2026-07-09 18:51:35 +0530

    djsdf

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -17,3 +17,13 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log("Async Task resolved")
 })
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
